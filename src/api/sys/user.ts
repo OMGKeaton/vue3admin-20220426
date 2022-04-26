390c963155e3413cec530e70b315605abd12776b
111 <844864184@qq.com>
2022-04-26 23:16:23 +0800

    初始化项目

---
+++ src/api/sys/user.ts
@@ -0,0 +1,41 @@
+import { defHttp } from '/@/utils/http/axios';
+import { LoginParams, LoginResultModel, GetUserInfoModel } from './model/userModel';
+
+import { ErrorMessageMode } from '/#/axios';
+
+enum Api {
+  Login = '/admin/acl/index/login',
+  Logout = '/admin/acl/index/logout',
+  GetUserInfo = '/admin/acl/index/info',
+  GetPermCode = '/getPermCode',
+}
+
+/**
+ * @description: user login api
+ */
+export function loginApi(params: LoginParams, mode: ErrorMessageMode = 'modal') {
+  return defHttp.post<LoginResultModel>(
+    {
+      url: Api.Login,
+      params,
+    },
+    {
+      errorMessageMode: mode,
+    },
+  );
+}
+
+/**
+ * @description: getUserInfo
+ */
+export function getUserInfo() {
+  return defHttp.get<GetUserInfoModel>({ url: Api.GetUserInfo }, { errorMessageMode: 'none' });
+}
+
+export function getPermCode() {
+  return defHttp.get<string[]>({ url: Api.GetPermCode });
+}
+
+export function doLogout() {
+  return defHttp.post({ url: Api.Logout });
+}
